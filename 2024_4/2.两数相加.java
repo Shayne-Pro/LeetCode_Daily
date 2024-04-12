6bdae6835b39d05167e0c210d3b86928f656245f
YanSun <yan.sun@msn.com>
2024-04-12 11:10:19 +0800

    code format adjustment

--- 2024_4/2.两数相加.java
+++ 2024_4/2.两数相加.java
@@ -1,27 +1,28 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		return add(l1, l2, 0);
 	}
+
 	public ListNode add(ListNode l1, ListNode l2, int bit) {
-		if(l1 == null && l2 == null && bit == 0) {
+		if (l1 == null && l2 == null && bit == 0) {
 			return null;
 		}
 		int val = bit;
-		if(l1 != null) {
+		if (l1 != null) {
 			val += l1.val;
 			l1 = l1.next;
 		}
-		if(l2 != null) {
+		if (l2 != null) {
 			val += l2.val;
 			l2 = l2.next;
 		}
--- 2024_4/9.回文数.java
+++ 2024_4/9.回文数.java
@@ -3,17 +3,17 @@ class Solution {
 		int new_v = 0;
 		int old = x;
 		int t = 0;
-		if(x < 0) {
+		if (x < 0) {
 			return false;
 		}
-		while(x != 0) {
+		while (x != 0) {
 			t = x % 10;
 			new_v = new_v * 10 + t;
 			x = (x - t) / 10;
 		}
-		if(new_v == old) {
+		if (new_v == old) {
 			return true;
-		}
-		else return false;
+		} else
+			return false;
 	}
 }
